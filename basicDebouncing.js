a09b2e61ea4c6f7f1137c4ae1d98f14fd4f55855
Vedant Sharma <vedant.sharma200@gmail.com>
2025-03-02 12:35:12 +0530

    Create basicDebouncing.js

---
+++ basicDebouncing.js
@@ -0,0 +1,26 @@
+// basic debouncing.
+
+function debounce(fn, delay) {
+    let timer;
+    return function(...args) {
+        let context=this;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            fn.apply(context, args);
+        }, delay);
+    }
+}
+
+const sayHello = () => console.log('hello');
+
+const deboundedMethod = debounce(sayHello, 300);
+
+deboundedMethod();
+deboundedMethod();
+deboundedMethod();
+deboundedMethod();
+
+setTimeout(() => deboundedMethod(), 400);
+
+// hello --- invoked as it was last call, and after delay of 300ms it got invoked.
+// hello ---- it got triggered after 400ms, and after that there was no furter call upto 300ms to clear this, hence called
